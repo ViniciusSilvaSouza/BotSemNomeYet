3e4349f5c4d1170820eb06e0870cb1f19f4b46cc
Vinicius Soares Silva <viniciussss@outlook.com.br>
2020-05-03 22:35:32 -0300

    comentando trabalho não finalizado

--- commands/play.js
+++ commands/play.js
@@ -1,6 +1,6 @@
 exports.run = async (client, message, args, tools) => {
     const serverQueue = queue.get(message.guild.id);
-    
+    //TODO: continuar a partir daqui - descobrir como fazer para repassar o execute para o funcions.js
     tools.play(args, serverQueue).catch(console.error);
     
 
